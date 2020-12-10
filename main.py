621d52363d3fd84560be92d3cc3b4b5164cea823
et6341 <72764888+et6341@users.noreply.github.com>
2020-12-10 13:09:25 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ while True:
     #if light level is less than 10, then set all neopixels on
     else:
         light.set_all(color.rgb(0, 0, 255))
+while True: 
+    print("light level: " + input.light_level())
+    if input.light_level() <6:
+        light.set_all(light.rgb(0, 0, 255))
+    elif input.light_level() <13:
+        light.set_all(light.rgb(255, 255, 0))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -9,3 +9,14 @@ while (true) {
     }
     
 }
+while (true) {
+    console.log("light level: " + input.lightLevel())
+    if (input.lightLevel() < 6) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else if (input.lightLevel() < 13) {
+        light.setAll(light.rgb(255, 255, 0))
+    } else {
+        light.clear()
+    }
+    
+}
